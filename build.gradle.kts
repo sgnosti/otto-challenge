2ccfbec8bb8876c874372255636c58980114dc28
Sara GN <saragn@gmail.com>
2023-02-27 22:06:09 +0100

    Add OpenAPI support

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,9 @@
 plugins {
 	java
-	id("org.springframework.boot") version "3.0.0"
+	id("org.springframework.boot") version "2.7.0"
 	id("io.spring.dependency-management") version "1.1.0"
-	id ("com.google.cloud.tools.jib") version "3.3.1"
+	id("com.google.cloud.tools.jib") version "3.3.1"
+	id("org.springdoc.openapi-gradle-plugin") version "1.6.0"
 }
 
 group = "de.otto.logistics"
@@ -17,10 +18,16 @@ configurations {
 
 repositories {
 	mavenCentral()
+    mavenLocal()
+    gradlePluginPortal()
 }
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
+	implementation("org.springdoc:springdoc-openapi-webflux-ui:1.6.14")
+	implementation("io.swagger.core.v3:swagger-annotations:2.2.8")
+    implementation("io.swagger.core.v3:swagger-models:2.2.8")
+
 	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
@@ -29,4 +36,11 @@ dependencies {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+openApi {
+    apiDocsUrl.set("http://localhost:8081/v3/api-docs")
+    outputDir.set(file("$buildDir/docs"))
+    outputFileName.set("swagger.json")
+    waitTimeInSeconds.set(30)
 }
--- src/main/java/de/otto/logistics/challenge/health/HealthCheckRouter.java
+++ src/main/java/de/otto/logistics/challenge/health/HealthCheckRouter.java
@@ -1,15 +1,22 @@
 package de.otto.logistics.challenge.health;
 
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+
+import org.springdoc.core.annotations.RouterOperation;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
-import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
-import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 
 @Configuration(proxyBeanMethods = false)
 public class HealthCheckRouter {
@@ -18,6 +25,13 @@ public class HealthCheckRouter {
     private String route;
 
     @Bean
+    @RouterOperation(path = "/health", produces = {
+        MediaType.APPLICATION_JSON_VALUE},
+        beanClass = HealthCheckHandler.class, method = RequestMethod.GET, beanMethod = "health",
+        operation = @Operation(operationId = "health", responses = {
+            @ApiResponse(responseCode = "200", description = "successful operation",
+                content = @Content(schema = @Schema(implementation = String.class)))}
+        ))
     public RouterFunction<ServerResponse> healthCheckRoute(HealthCheckHandler healthCheckHandler) {
         return RouterFunctions.route(GET(route).and(accept(MediaType.TEXT_PLAIN)), healthCheckHandler::health);
     }
